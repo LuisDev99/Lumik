7478ec26c622bb14b44dc30a59d34738e4568881
Luis Gerardo <luisgpr99@gmail.com>
2020-11-07 22:41:26 -0600

    Instalado paquete que traslada Voz a Texto. Agregado una flags en el tsconfig.json que arregla un bug en el compilador de Typescript version 4

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="react-scripts" />
+
+declare module "react-click-n-hold" {
+  import React from "react";
+  
+  interface ClickNHoldProps {
+    time?: number;
+    children?: React.ReactNode;
+    onStart?: () => void;
+    onClickNHold?: () => void;
+    onEnd?: () => void;
+  }
+
+  export default class ClickNHold extends React.Component<ClickNHoldProps> { }
+}
